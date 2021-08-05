3a5c9c00d916aa61166bd5d725ad0486eab61e89
Saurabh Sharma <saurabh182000@gmail.com>
2021-08-05 12:04:41 +0530

    MainActivity Modified

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.newsapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView headingView;
+    String title;
+    String API_KEY = "501582e84e1b45cca73d00d432de5aab";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_card);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://newsapi.org/v2/top-headlines?country=in", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Saurabh", "onResponse: Everything is good " + response);
+//
+//                try {
+//                    JSONArray articles = response.getJSONArray("articles");
+//                    JSONObject news1 = articles.getJSONObject(0);
+//                    title = news1.getString("title");
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Saurabh", "onErrorResponse: Something Went Wrong " + error);
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("X-Api-Key", "501582e84e1b45cca73d00d432de5aab");
+                return params;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
+
+        headingView = findViewById(R.id.news_heading);
+//        headingView.setText(title);
     }
 }
