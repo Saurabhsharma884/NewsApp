14e30b22d536070a1f639a675a52d58ed53e4c1c
Saurabh Sharma <saurabh182000@gmail.com>
2021-08-07 23:05:14 +0530

    API update

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -6,10 +6,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,15 +19,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
 
     String img_url;
     String title;
-    String URL = "https://newsdata.io/api/1/news?country=in";
-    String API_KEY = "pub_732b63001661236ad6efdff3d7901657d36";
+    String desc;
+    String URL = "https://gnews.io/api/v4/top-headlines?country=in&lang=en&token=";
+    String API_KEY = "192e30720fae3e9854a83bfaac83a8bc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +33,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.news_card);
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL + API_KEY, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 //                Log.d("Saurabh", "onResponse: Everything is good " + response);
 
                 try {
-                    JSONArray results = response.getJSONArray("results");
+                    JSONArray results = response.getJSONArray("articles");
                     JSONObject news1 = results.getJSONObject(0);
                     title = news1.getString("title");
-                    img_url = news1.getString("image_url");
-                    Log.d("Saurabh", "onResponse: " + title);
-                    setData(title, img_url);
+                    img_url = news1.getString("image");
+                    desc = news1.getString("description");
+//                    Log.d("Saurabh", "onResponse: " + title);
+                    setData(title, img_url, desc);
 
 
                 } catch (JSONException e) {
@@ -63,24 +59,16 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Saurabh", "onErrorResponse: Something Went Wrong " + error);
 
             }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("X-ACCESS-KEY", API_KEY);
-
-                return params;
-            }
-        };
+        });
         requestQueue.add(jsonObjectRequest);
 
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
 
     }
 
-    private void setData(String title, String img_url) {
+    private void setData(String title, String img_url, String desc) {
 
         ImageView imageView = findViewById(R.id.news_img);
 
@@ -99,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
             title = title.substring(0, 50);
 
         headingView.setText(title + "...");
+
+        TextView descriptionView = findViewById(R.id.news_description);
+        descriptionView.setText(desc);
 
     }
 }
