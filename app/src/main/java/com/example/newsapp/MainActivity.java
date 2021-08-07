17c64daff2535e598dfa57bc954a1b04e1c8c167
Saurabh Sharma <saurabh182000@gmail.com>
2021-08-08 00:41:27 +0530

    Adapter updated

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.newsapp;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
@@ -14,7 +13,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
     String URL = "https://gnews.io/api/v4/top-headlines?country=in&lang=en&token=";
     String API_KEY = "192e30720fae3e9854a83bfaac83a8bc";
 
-    ArrayList<News> NewsArticles;
+    ArrayList<News> NewsArticles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_card);
+        setContentView(R.layout.activity_main);
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL + API_KEY, null, new Response.Listener<JSONObject>() {
@@ -67,31 +65,11 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        NewsAdapter newsAdapter = new NewsAdapter(NewsArticles);
+        recyclerView.setAdapter(newsAdapter);
 
     }
 
-    private void setData(String title, String img_url, String desc) {
 
-        ImageView imageView = findViewById(R.id.news_img);
-
-        if (img_url == null) {
-            imageView.setImageResource(R.drawable.image_not_available);
-        } else {
-            Glide.with(this)
-                    .load(img_url)
-                    .centerCrop()
-                    .error(R.drawable.ic_launcher_foreground)
-                    .into(imageView);
-        }
-
-        TextView headingView = findViewById(R.id.news_heading);
-        if (title.length() > 50)
-            title = title.substring(0, 50);
-
-        headingView.setText(title + "...");
-
-        TextView descriptionView = findViewById(R.id.news_description);
-        descriptionView.setText(desc);
-
-    }
 }
--- app/src/main/java/com/example/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/newsapp/NewsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.newsapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private ArrayList<News> mNewsArray;
+    private Context context;
 
     public NewsAdapter(ArrayList<News> mNewsArray) {
         this.mNewsArray = mNewsArray;
@@ -21,11 +25,28 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+
+        private final TextView headingTextView;
+        private final TextView descriptionTextView;
+        private final ImageView newsImageView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ImageView newsImage = itemView.findViewById(R.id.news_img);
-            TextView headingText = itemView.findViewById(R.id.news_heading);
-            TextView descriptionText = itemView.findViewById(R.id.news_description);
+            newsImageView = itemView.findViewById(R.id.news_img);
+            headingTextView = itemView.findViewById(R.id.news_heading);
+            descriptionTextView = itemView.findViewById(R.id.news_description);
+        }
+
+        public TextView getHeadingTextView() {
+            return headingTextView;
+        }
+
+        public TextView getDescriptionTextView() {
+            return descriptionTextView;
+        }
+
+        public ImageView getNewsImageView() {
+            return newsImageView;
         }
     }
 
@@ -33,18 +54,39 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_card, parent, false);
+        context = parent.getContext();
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        News news = mNewsArray.get(position);
 
+        String title = news.getTitle();
+        String imgUrl = news.getImg_url();
+        String desc = news.getDesc();
+
+        if (title.length() > 50)
+            title = title.substring(0, 50);
+        title = title + "...";
+        holder.getHeadingTextView().setText(title);
+
+        holder.getDescriptionTextView().setText(desc);
+
+        if (imgUrl == null) {
+            holder.getNewsImageView().setImageResource(R.drawable.image_not_available);
+        } else {
+            Glide.with(context)
+                    .load(imgUrl)
+                    .centerCrop()
+                    .error(R.drawable.ic_launcher_foreground)
+                    .into(holder.getNewsImageView());
+        }
     }
 
     @Override
     public int getItemCount() {
         return 0;
     }
-
 
 }
