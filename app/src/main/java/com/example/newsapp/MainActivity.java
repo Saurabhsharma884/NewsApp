cd2838a9e461f464118b761e7e79716ecbf12132
Saurabh Sharma <saurabh182000@gmail.com>
2021-09-02 00:59:05 +0530

    set Click Listener

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     boolean NEWS_LOADED = false;
     int NETWORK_ERROR = 0;
     int VOLLEY_ERROR = 1;
+    boolean CONNECTED = false;
 
     ArrayList<News> NewsArticles = new ArrayList<>();
     RequestQueue requestQueue;
@@ -56,16 +57,11 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                checkConnectivity();
+                CONNECTED = checkConnectivity();
             }
         });
-    }
 
-    private void checkConnectivity() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+        if (CONNECTED) {
             if (!NEWS_LOADED)
                 makeRequest();
             else {
@@ -73,10 +69,32 @@ public class MainActivity extends AppCompatActivity {
                 swipeRefreshLayout.setRefreshing(false);
             }
         } else {
-
             alertDialog(NETWORK_ERROR);
             swipeRefreshLayout.setRefreshing(false);
         }
+
+
+        ListView listView = findViewById(R.id.news_list);
+        NewsAdapter newsAdapter = new NewsAdapter(this, NewsArticles);
+        listView.setAdapter(newsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                News cur = NewsArticles.get(position);
+
+                Log.d("saurabh", "onItemClick: item clicked was " + cur.getmTitle());
+
+            }
+        });
+
+    }
+
+    private boolean checkConnectivity() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
     }
 
     private void alertDialog(int error) {
@@ -85,12 +103,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeRequest() {
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL + API_KEY, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 //                Log.d("Saurabh", "onResponse: Everything is good " + response);
                 try {
-                    getDataFromResponse(response);
+                    JSONArray articles = response.getJSONArray("articles");
+                    makeArticleList(articles);
                     swipeRefreshLayout.setRefreshing(false);
                     NEWS_LOADED = true;
                 } catch (JSONException e) {
@@ -108,28 +128,8 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
-    private void getDataFromResponse(JSONObject response) throws JSONException {
-        JSONArray articles = response.getJSONArray("articles");
-        NewsArticles = makeArticleList(articles);
-//        Log.d("Saurabh", "onCreate: "+NewsArticles);
 
-        ListView listView = findViewById(R.id.news_list);
-        NewsAdapter newsAdapter = new NewsAdapter(this, NewsArticles);
-        listView.setAdapter(newsAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                News cur = NewsArticles.get(position);
-
-                Log.d("saurabh", "onItemClick: item clicked was " + cur.getmTitle());
-
-            }
-        });
-    }
-
-    private ArrayList<News> makeArticleList(JSONArray articles) throws JSONException {
+    private void makeArticleList(JSONArray articles) throws JSONException {
 
         for (int i = 0; i < 10; i++) {
             JSONObject news = articles.getJSONObject(i);
@@ -140,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
             NewsArticles.add(new News(title, img_url, desc, news_url));
 //                    Log.d("Saurabh", "onResponse: " + title);
         }
-        return NewsArticles;
     }
 
 
