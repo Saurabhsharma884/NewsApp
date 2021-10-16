065d15b4647f893f24dba5eb9f4f888a27c0e482
Saurabh Sharma <saurabh182000@gmail.com>
2021-10-16 13:10:26 +0530

    preferences

--- app/src/main/java/com/example/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/newsapp/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.newsapp;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayShowHomeEnabled(true);
     }
 }
